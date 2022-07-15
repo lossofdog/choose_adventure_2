ade515862a8839387f6dd1fd33d7cea599be73ff
Ian <iantwelve@gmail.com>
2022-07-14 20:59:12 -0700

    made updates

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 //Your Code here
+//Your Code here
+//this stores the response as a variable and holds on to it
+let response = window.prompt("do you go left or right")
+
+console.log(response)
+
+
+
+if(response === "left"){
+    console.log("user submitted left")
+} else if (response === "right"){
+    console.log("user submitted right")
+} else {
+    console.log("follow the instructions!!")
+}
+
+let response2 = window.prompt("do you go up or down")
+
+console.log(response2)
+
+if(response2 === "up"){
+    console.log("user submitted up")
+} else if (response2 === "down"){
+    console.log("user submitted down")
+} else {
+    console.log("follow the instructions!!")
+}
